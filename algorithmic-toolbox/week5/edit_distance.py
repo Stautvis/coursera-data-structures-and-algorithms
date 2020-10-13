bafce0cfda6592db4778519469bfc4e2ffd6c45f
Tautvydas Steponavičius <tutis@Tautvydass-Air.Dlink>
2020-10-13 15:53:35 +0300

    edit_distance.py done

--- algorithmic-toolbox/week5/edit_distance.py
+++ algorithmic-toolbox/week5/edit_distance.py
@@ -1,7 +1,25 @@
 # Uses python3
 def edit_distance(s, t):
-    #write your code here
-    return 0
+    sl = len(s)
+    tl = len(t)
+    #return editDistDP(s, t, sl, tl)
+    table = [[0 for x in range(tl + 1)] for x in range(sl + 1)] 
+
+    for si in range(sl + 1):
+        for ti in range(tl + 1):
+            if si == 0:
+                table[si][ti] = ti
+            elif ti == 0:
+                table[si][ti] = si
+            elif s[si - 1] == t[ti - 1]:
+                table[si][ti] = table[si - 1][ti - 1]
+            else:
+                table[si][ti] = 1 + min(
+                    table[si - 1][ti],
+                    table[si - 1][ti - 1],
+                    table[si][ti - 1]
+                )
+    return table[sl][tl]
 
 if __name__ == "__main__":
     print(edit_distance(input(), input()))

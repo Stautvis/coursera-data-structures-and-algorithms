0381042f4fe9873498839bde972a60181048c293
Tautvydas Steponavičius <tutis@Tautvydass-Air.Dlink>
2020-10-13 15:46:55 +0300

    lcs3.py done

--- algorithmic-toolbox/week5/lcs3.py
+++ algorithmic-toolbox/week5/lcs3.py
@@ -7,8 +7,9 @@ def lcs3(a, b, c):
     m = len(b)
     l = len(c)
 
-    if 0 in [n, m, l]: return 0 #if any sequence has no elements, then there isn't common subsequence
-    lcs = [[[0] * (l + 1)] * (m + 1)] * (n + 1)
+
+    if 0 in [n, m, l]: return 0 #if either one sequence has no elements, then there isn't common subsequence
+    lcs = [[[0 for i in range(l+1)] for j in range(m+1)] for k in range(n+1)] 
 
     for ni in range(1,n + 1):
         for mi in range(1,m + 1):
@@ -16,9 +17,11 @@ def lcs3(a, b, c):
                 if a[ni - 1] == b[mi - 1] and a[ni - 1] == c[li - 1]:
                     lcs[ni][mi][li] = lcs[ni - 1][mi - 1][li - 1] + 1
                 else:
-                    lcs[ni][mi][li] =   max(max(lcs[ni - 1][mi][li],
-                                            lcs[ni][mi - 1][li]),
-                                            lcs[ni][mi][li -1])
+                    lcs[ni][mi][li] =   max(
+                        lcs[ni - 1][mi][li],
+                        lcs[ni][mi - 1][li],
+                        lcs[ni][mi][li -1]
+                    )
     return lcs[n][m][l]
 
 

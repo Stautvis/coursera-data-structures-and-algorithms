0ad580e9ac18727340b23179d72c8413ea21d76f
Tautvydas Steponavičius <tutis@Tautvydass-Air.Dlink>
2020-10-30 15:00:11 +0200

    covering_segments.py done

--- algorithmic-toolbox/week3/covering_segments.py
+++ algorithmic-toolbox/week3/covering_segments.py
@@ -6,11 +6,15 @@ Segment = namedtuple('Segment', 'start end')
 
 def optimal_points(segments):
     points = []
-    #write your code here
-    for s in segments:
-        points.append(s.start)
-        points.append(s.end)
+    while len(segments) > 0:
+        min_point = min([x.end for x in segments])
+        print(min_point)
+        points.append(min_point)
+        segments = list(filter(lambda x: x.start > min_point or min_point > x.end, segments))
+        print(segments)
     return points
+
+    
 
 if __name__ == '__main__':
     input = sys.stdin.read()

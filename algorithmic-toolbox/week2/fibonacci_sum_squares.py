7c9e97560f9c6fc9b7617110bdd131ba79b6d665
Tautvydas Steponavičius <tutis@Tautvydass-MacBook-Air.local>
2020-11-13 14:27:03 +0200

    fibonacci_sum_squares.py done

--- algorithmic-toolbox/week2/fibonacci_sum_squares.py
+++ algorithmic-toolbox/week2/fibonacci_sum_squares.py
@@ -1,19 +1,28 @@
 # Uses python3
 from sys import stdin
 
-def fibonacci_sum_squares_naive(n):
-    if n <= 1:
-        return n
+def pisano_period(m):
+    previous = 0
+    current = 1
 
+    for i in range(m*m):
+        previous, current = current, (current + previous) % m
+        if previous == 0 and current == 1:
+            return i + 1
+
+def calc_fib(n):
+    sum = 0
     previous = 0
     current  = 1
-    sum      = 1
-
-    for _ in range(n - 1):
+    
+    for i in range(n + 1):
+        sum += (previous * previous) % 10
         previous, current = current, (previous + current) % 10
-        sum += (current % 10)^2
 
-    return sum % 10
+    return  sum % 10
+
+def fibonacci_sum_squares_naive(n):
+    return calc_fib(n % pisano_period(10))
 
 n = int(input())
 print(fibonacci_sum_squares_naive(n))

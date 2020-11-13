666f82d5f2172212f321a0cfe566aa6c999a20ff
Tautvydas Steponavičius <tutis@Tautvydass-MacBook-Air.local>
2020-11-13 15:26:25 +0200

    different_summands.py done

--- algorithmic-toolbox/week3/different_summands.py
+++ algorithmic-toolbox/week3/different_summands.py
@@ -1,9 +1,17 @@
 # Uses python3
 import sys
+import math
+from typing import SupportsAbs
 
 def optimal_summands(n):
     summands = []
-    #write your code here
+    k=int((math.sqrt(1+(8*n))-1)/2)
+    diff=int(n-((k-1)*k/2))
+
+    for i in range(k - 1):
+        summands.append(i+1);
+    summands.append(diff)
+
     return summands
 
 if __name__ == '__main__':
